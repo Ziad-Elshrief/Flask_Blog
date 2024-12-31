f187745f2828402f54625a54461d24b476ecfca9
Ziad Elshrief <zyad2100@gmail.com>
2024-12-31 14:04:33 +0200

    inital commit

---
+++ Flask_blog/config.py
@@ -0,0 +1,9 @@
+
+class Config:
+    SECRET_KEY= '9fK93paq7v5LbbneUAbtR7177JMadSCy'
+    SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
+    MAIL_SERVER='smtp.googlemail.com'
+    MAIL_PORT=587
+    MAIL_USE_TLS=True
+    MAIL_USERNAME='@'
+    MAIL_PASSWORD='password'
